7b4f9fef9155be5afa3eb427792f7e2e2ed3677a
grebennikov_iy <grebennikov_iy@magnit.ru>
2021-07-28 18:46:45 +0300

    first commit

---
+++ polls/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    correct = models.BooleanField()
+
+    def __str__(self):
+        return self.choice_text
+
+
+class UserQuestion(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    question = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
+    passed = models.BooleanField()
